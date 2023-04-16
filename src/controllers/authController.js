f6ac61d886a04b3217ddd07bd863b186472b8ef4
anganevagg <anganevagg@protonmail.com>
2023-04-15 19:16:17 -0600

    fixed bugs in auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,9 +6,15 @@ class AuthController {
   login = async (req, res, next) => {
     try {
       const { email, password } = req.body
-      const response = await this.Service.checkUser({
+      const response = await this.Service.checkUser(
         email,
         password
+      )
+      res.json({
+        message: 'Logged in',
+        data: {
+          id: response.id
+        }
       })
     } catch (error) {
       next(error)
@@ -17,16 +23,10 @@ class AuthController {
   signUp = async (req, res, next) => {
     try {
       const { email, password, first_name, last_name } = req.body
-      const userData = {
-        email,
-        password,
-        first_name,
-        last_name
-      }
-      const response = await this.Service.createUser(userData)
+      const response = await this.Service.createUser(email, password, first_name, last_name)
       res.json({
         message: 'User created',
-        user: {
+        data: {
           id: response.id
         }
       })
--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ const ErrorHandler = (error, req, res, next) => {
   res.json({
     msg: error.message,
     data: null
-  })
+  }).status(error.status)
 }
 
 module.exports = ErrorHandler
